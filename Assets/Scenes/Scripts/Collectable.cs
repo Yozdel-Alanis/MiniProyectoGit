f4e498fc5acffb76861d47316c03a37ef7258746
Templeanarue <60400923+Templeanarue@users.noreply.github.com>
2020-06-18 16:45:16 -0500

    Añadi moneda, materiales y follow player:)

--- Assets/Scenes/Scripts/Collectable.cs
+++ Assets/Scenes/Scripts/Collectable.cs
@@ -1,28 +1,27 @@
 ﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    public AudioClip clip;
+
+   
 
     public float rotationSpeed;
-
     private void Update()
     {
-        //Quaternion
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f,rotationSpeed * Time.deltaTime,0f));
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, rotationSpeed * Time.deltaTime, 0f));
     }
     private void OnTriggerEnter(Collider other)
     {
-        AudioSource.PlayClipAtPoint(clip, this.transform.position);
 
 
-        //this.gameObject.SetActive(false);
-        Destroy(this.gameObject);
-
-
+        this.gameObject.SetActive(false);
+        Destroy(gameObject);
         GameManager gameManager = FindObjectOfType<GameManager>();
         gameManager.AddCoin();
+
+        Debug.Log("MONEDA:" + this.gameObject.name);
     }
 }
